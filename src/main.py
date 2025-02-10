73d5a67501b76fb0d912c2d61897be5ef9c4b741
thmastin <thmastin@gmail.com>
2025-02-10 07:48:12 -0500

    Add test case to test TextNode implementation

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    #Create a test node and print it to verify TextNode implmentation
+    textnode = TextNode("This is a text node", TextType.BOLD, "https://example.com")
+    print(textnode)
+
+if __name__ == "__main__":
+    main()
