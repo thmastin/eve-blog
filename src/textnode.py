9023d769626565e61313c805e6c458518b656586
thmastin <thmastin@gmail.com>
2025-02-10 07:22:44 -0500

    Add __eq__ check to Textnode

--- src/textnode.py
+++ src/textnode.py
@@ -14,4 +14,12 @@ class Textnode(text, text_type, url=None):
         self.text_type = text_type
         self.url = url
 
-    
+    def __eq__(other, Textnode):
+        if isinstance(other, Textnode):
+            return (
+                self.text == other.text and
+                self.text_type == other.text_type and
+                self.url == other.url
+            )
+        return False
+        
