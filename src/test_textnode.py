54b75eabb634d50eb921462a5791bc876484b98f
thmastin <thmastin@gmail.com>
2025-02-10 14:56:38 -0500

    Add more test conditions

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -14,6 +14,21 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.ITALIC)
         self.assertNotEqual(node, node2)
 
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://example.com")
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://example.com")
+        self.assertEqual(node, node2)
+
+    def test_not_eq_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, None)
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://example.com")
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node that is not equal", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
