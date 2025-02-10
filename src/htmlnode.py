70a02a19f094012766f19e0b83aee398596de4d6
thmastin <thmastin@gmail.com>
2025-02-10 15:00:04 -0500

    Add HTMLNode class and constructor

---
+++ src/htmlnode.py
@@ -0,0 +1,8 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    
