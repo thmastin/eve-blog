1204dd15022347059db7179017cda4995d31c2fa
thmastin <thmastin@gmail.com>
2025-02-11 00:02:28 -0500

    Add comments

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -28,3 +28,4 @@ class HTMLNode:
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, props)
+    """Represents a single HTML tag with no children"""
