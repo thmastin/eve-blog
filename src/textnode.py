94dcad2b4ddd98b0595be0f60f7783f5fe24df6e
thmastin <thmastin@gmail.com>
2025-02-14 11:52:41 -0500

    Handle remaining types in cases

--- src/textnode.py
+++ src/textnode.py
@@ -38,6 +38,16 @@ def text_node_to_html_node(text_node):
     match text_node.text_type:
         case TextType.TEXT:
             return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINKS:
+            return LeafNode("a", text_node.text,text_node.props["href"] )
+        case TextType.IMAGES:
+            return LeafNode("img", "", text_node["src", "alt"])
         case _:
             raise Exception(f"Invalid text type: {text_node.text_type}")
         
