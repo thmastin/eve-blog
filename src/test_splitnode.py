6026d5eebca77fce2846d1fb74e996097e89a282
thmastin <thmastin@gmail.com>
2025-02-15 18:12:09 -0500

    Add test for a single delimeter with no closing delimeter

--- src/test_splitnode.py
+++ src/test_splitnode.py
@@ -16,6 +16,19 @@ class TestSplitNodes(unittest.TestCase):
         # Assert equality
         self.assertEqual(actual_output, expected_output)
     
+    def test_no_closing_delimiter(self):
+        # Input: A single TextNode with an opening bold delimiter but no closing
+        node = TextNode("This is normal. **This is bold. This is normal", TextType.TEXT)
+
+        # Expect the function to raise an exception
+        with self.assertRaises(Exception) as context:
+            split_nodes_delimiter([node], "**", TextType.BOLD)
+
+        # Check if the exception message matches
+        self.assertEqual(
+            str(context.exception), "Invalid Markdown Syntax: Must have closing **"
+        )
+    
 
     def test_placeholder(self):
         pass
