7a903af14fc73bae7d6bd3db1c7217d4ed64d1b3
thmastin <thmastin@gmail.com>
2025-03-01 00:47:47 -0500

    Add function to return BlockType and implement for Heading and Code

--- src/blocktype.py
+++ src/blocktype.py
@@ -1,4 +1,16 @@
 from enum import Enum
 
-class BlockType(enum):
-    pass
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def block_to_block_type(block):
+    split = block.split(" ")
+    if split[0].startswith("#") and 1 <= split[0].count("#") <= 6:
+        return BlockType.HEADING
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
