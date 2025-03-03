6e15df2fd916f75231381938ccb86f5a166a6717
thmastin <thmastin@gmail.com>
2025-03-02 20:14:20 -0500

    Fix source and destination directories

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from htmlnode import HTMLNode, LeafNode
 from copydirectory import copy_directory
 
 def main():
-    copy_directory("src", "static")
+    copy_directory("static", "public")
 
 if __name__ == "__main__":
     main()
